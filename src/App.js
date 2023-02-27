0cc57dc3fa39cd138c057bfa00b6e4cd88844b93
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-27 12:26:09 -0600

    Add App.css, omdb API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import { useEffect} from "react";
+
+const API_URL = 'http://www.omdbapi.com?apikey=4cd4f63d';
+
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
     return (
         <h1>App</h1>
     );
